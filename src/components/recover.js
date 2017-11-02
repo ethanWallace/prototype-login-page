da43c952feab80455d8d4f511116d69eb7b75f13
piet0024 <n.pietrantonio0024@live.com>
2017-11-02 15:34:49 -0400

    recover password form

--- src/components/recover.js
+++ src/components/recover.js
@@ -4,18 +4,63 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
 
+const style = {
+    submitInput:{
+        cursor: 'pointer',
+        position: 'absolute',
+        top: 0,
+        bottom: 0,
+        right: 0,
+        left: 0,
+        width: '100%',
+        opacity: 0,
+    }
+
+}
+
 class Recover extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            emailSent:null,
+            value:''
+        };
+        this.sendRecoveryEmail = this.sendRecoveryEmail.bind(this);
+    }
+
+    sendRecoveryEmail(event){
+        //Handle the event here
+        event.preventDefault();
+        this.setState({
+            emailSent:true
+        });
+        
+    }
     render(){
         return(
             <div>
-                <MuiThemeProvider>
                     <h1>Recover Your Password</h1>
-                    <p>Enter your Email or Usersname and we will send you an email to recover your password</p>
-                    <TextField 
-                        floatingLabelText="Username of Email"
-                    />
-                    <RaisedButton label="Reover Password" fullWidth={true} />
-                </MuiThemeProvider>
+                    {!this.state.emailSent &&
+                    <div>
+                        <p>Enter your Email or Usersname and we will send you an email to recover your password</p>
+                        <form onSubmit={this.sendRecoveryEmail}>
+                        <TextField 
+                            floatingLabelText="Username of Email"
+                            value = {this.state.value}
+                        />
+                        <RaisedButton 
+                            label="Recover Password" 
+                            fullWidth={true} 
+                            containerElement="label"
+                        >
+                            <input type="submit" style={style.submitInput} />
+                        </RaisedButton>
+                        </form>
+                    </div>}
+                    {this.state.emailSent &&
+                    <div>
+                        <p>The email has been sent!</p>
+                    </div>}
             </div>
         );
     }
